67134c52e0089e8d3a41edb6ee6a9b73d177b97e
Jarod Wuillaume <wuillaume.ja@gmail.com>
2022-06-27 17:06:31 +0200

    api_pole_emploi_ok

--- src/app/auth/services/tokens.service.ts
+++ src/app/auth/services/tokens.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Data } from 'src/app/buisness/offer/models/data';
 
 @Injectable({
@@ -13,18 +13,23 @@ export class TokensService {
   constructor(
     private httpClient: HttpClient,
   ) 
-  { 
-    this.token.subscribe(token => {
-      localStorage.setItem('token', token)
-    })
-    
+  {
+    // je recupere le token de mon localStorage
     const token = localStorage.getItem('token')
-    if (token) this.setToken(token)
-    else this.httpClient.get<string>('http://localhost:3000').subscribe((tkn : any) => {
-      this.setToken(tkn.access_token)
+    if (token === null)
+    {
+      // si je n est pas de token je le recup un sur mon api
+      this.httpClient.get<string>('http://localhost:3000').subscribe((tkn : any) => {
+        // j'envoi le token recpu dans mon behavior subject et dans mon localStorage
+        localStorage.setItem('token', tkn.access_token)
+        this.setToken(tkn.access_token)
+      }
+      )
     }
-    )
-    
+    else if (token !== null){
+      // j'envoi le token de mon localStorage dans mon behavior subject
+      this.setToken(token)
+    }
   }
 
   getToken(): Observable<string> {
--- src/app/buisness/offer/services/offer.service.ts
+++ src/app/buisness/offer/services/offer.service.ts
@@ -1,7 +1,7 @@
 /* It's a service that manages the offers */
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, firstValueFrom, lastValueFrom, map, Observable, Subject } from 'rxjs';
 import { TokensService } from 'src/app/auth/services/tokens.service';
 import { ApiOffer } from '../models/apiOffer';
 import { Data } from '../models/data';
@@ -21,7 +21,7 @@ export class OfferService {
   can emit values to its subscribers. A BehaviorSubject is a Subject that emits the last value it
   received
   to its subscribers. */
-  private observableOffer = new BehaviorSubject<Offer[]>(this.offers);
+  private observableOffers = new BehaviorSubject<Offer[]>(this.offers);
   
   private API_URL = 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search'
 
@@ -30,19 +30,27 @@ export class OfferService {
     private tokensService: TokensService,
   ) { 
     console.log('OfferService created');
+    // j appel ma methode dans le constructeur pour recup les offres au lancement de mon app 
     this.fetchDataFromApi()
   } 
 
-  async fetchDataFromApi(): Promise<void> {
-    
+  // Cette methode permet de recuperer les offres de l'api de pole emploi
+  fetchDataFromApi(): void {
+
+    // je souscris a getToken() du token service 
     this.tokensService.getToken().subscribe(token => {
+      // je verifie que mon token n est pas nul
       if (token !== '') {
+        // je recupere les données de l'api
         this.httpClient.get<Offer[]>(this.API_URL,
+          // j ajoute un headers avec mon token
           { headers: { Authorization: `Bearer ${token}` } }
         )
         .subscribe(
           (e : any) => {
+            // je boucle les données
             e.resultats.forEach((o : any) => {
+              // je créé un nouvel objet offer
               let newOffer: Offer = (
                 {
                   id: o.id,
@@ -56,6 +64,7 @@ export class OfferService {
                   isOpen: false,
                 }
               )
+              // je l'ajoute à ma liste de mon BehaviorSubject 
               this.addOffer(newOffer);
             })
           }
@@ -70,23 +79,23 @@ export class OfferService {
   }
 
   getObservableOffers(): Observable<Offer[]> {
-    return this.observableOffer.asObservable();
+    return this.observableOffers.asObservable();
   }
 
   getObservableOffer(id: string): Observable<Offer | undefined> {
-    return this.observableOffer.asObservable()
+    return this.observableOffers.asObservable()
       .pipe(
         map(offers => offers.find(offer => offer.id === id))
       );
   }
 
   getSubjectOffer(): Subject<Offer[]> {
-    return this.observableOffer;
+    return this.observableOffers;
   }
 
   addOffer(offer: Offer): void {
-    this.observableOffer.getValue().push(offer);
-    this.getSubjectOffer().next(this.observableOffer.getValue());
+    this.observableOffers.getValue().push(offer);
+    this.getSubjectOffer().next(this.observableOffers.getValue());
   }
 
   updateOffer(offer: Offer[]): void {
@@ -98,45 +107,45 @@ export class OfferService {
   }
 
   blacklistedUnBlacklisteOffer(offer: Offer): void {
-    this.observableOffer.getValue().find(o => {
+    this.observableOffers.getValue().find(o => {
       if (o.id === offer.id) {
         o.isBlacklisted = !o.isBlacklisted;
         console.log(o)
       }
-      this.observableOffer.next(this.observableOffer.getValue());
+      this.observableOffers.next(this.observableOffers.getValue());
     })}
 
   favoriteUnFavoriteOffer(offer: Offer): void {
-      this.observableOffer.getValue().find(o => {
+      this.observableOffers.getValue().find(o => {
         if (offer.id === o.id) {
           o.isFavorite = !o.isFavorite;
           console.log(o)
         }
-        this.observableOffer.next(this.observableOffer.getValue());
+        this.observableOffers.next(this.observableOffers.getValue());
     })
   }
 
   applyOffer(offer: Offer): void {
-    this.observableOffer.getValue().find(o => {
+    this.observableOffers.getValue().find(o => {
       if (offer.id === o.id) {
         o.isApply = !o.isApply;
         console.log(o)
       }
-      this.observableOffer.next(this.observableOffer.getValue());
+      this.observableOffers.next(this.observableOffers.getValue());
   })
   }
 
   openOffer(offer: Offer): void {
-    this.observableOffer.getValue().find(o => {
+    this.observableOffers.getValue().find(o => {
       if (offer.id === o.id) {
         o.isOpen = !o.isOpen;
         console.log(o)
       }
-      this.observableOffer.next(this.observableOffer.getValue());
+      this.observableOffers.next(this.observableOffers.getValue());
     })}
 
   getBlackLidtOffer(): Observable<number> {
-    return this.observableOffer.asObservable()
+    return this.observableOffers.asObservable()
       .pipe(
         map(offers => offers.filter(o => o.isBlacklisted)),
         map(offers => offers.length)
